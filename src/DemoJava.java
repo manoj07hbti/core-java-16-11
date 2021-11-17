80f7f7cecf1980361a095f39918bff5192233dc8
60manojkumar@gmail.com <Motivated@2019>
2021-11-17 19:39:38 +0530

    Initial commit

---
+++ src/DemoJava.java
@@ -0,0 +1,13 @@
+//SYNTAX : class className{ }
+
+public class DemoJava {
+
+    public static void main (String args[]){
+
+    System.out.println("Hello Java ");
+    System.out.println("Hello World ");
+    System.out.println("Hello World 2");
+
+    }
+
+}
